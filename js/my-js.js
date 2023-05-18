263b213613275c03dc3977a43b82fd9f5e6c8839
vasil <vasilbazhiv12@gmail.com>
2023-05-18 13:36:37 +0300

    new-js-rev

--- js/my-js.js
+++ js/my-js.js
@@ -1,2 +1,17 @@
 //alert
-console.log(123) 
+const popupsAll = document.querySelector('.popups-order')
+document.querySelector('.create-tth').addEventListener('click', () => {
+	console.log(123)
+	popupsAll.classList.add('popups-order-create')
+})
+
+const switchPop = e => {
+	const elem = e.target
+	const one = elem.classList.contains('popups-order')
+	if (one) {
+		popupsAll.classList = ''
+		popupsAll.classList.add('popups-order')
+	}
+}
+
+popupsAll?.addEventListener('click', switchPop)
