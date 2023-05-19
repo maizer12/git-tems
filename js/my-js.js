254c25e9895f7190038acfa81e321097df6a5311
vasil <vasilbazhiv12@gmail.com>
2023-05-19 09:00:26 +0300

    new-func-orde

--- js/my-js.js
+++ js/my-js.js
@@ -1,17 +1,47 @@
-//alert
+//create tth popup
 const popupsAll = document.querySelector('.popups-order')
-document.querySelector('.create-tth').addEventListener('click', () => {
-	console.log(123)
-	popupsAll.classList.add('popups-order-create')
-})
+const popupCreateBtn = document.querySelector('.create-tth')
+const popupCreateCloseBtn = document.querySelector('.create-warehouse__close ')
 
+function switchPopCreate (e){
+	if(e){
+		popupsAll.classList.add('popups-order-create')
+	}else{
+		popupsAll.classList.remove('popups-order-create')
+	}
+}
 const switchPop = e => {
 	const elem = e.target
 	const one = elem.classList.contains('popups-order')
 	if (one) {
-		popupsAll.classList = ''
-		popupsAll.classList.add('popups-order')
+		switchPopCreate(false)
+	}
+}
+popupCreateCloseBtn?.addEventListener('click', ()=> switchPopCreate(false) )
+popupCreateBtn?.addEventListener('click', ()=> switchPopCreate(true))
+popupsAll?.addEventListener('click', switchPop)
+
+//client-pop
+const popupsCreateClient = document.querySelector('.popups-create-client')
+const buttonOpenAddClient = document.querySelector('.add-client-btn')
+const buttonCloseAddClient  = document.querySelector('.popups-create-client__close')
+
+function switchClientPop(a){
+	if(a){
+		popupsCreateClient.classList.add('popups-create-client-active')
+	}else{
+		popupsCreateClient.classList.remove('popups-create-client-active')
 	}
 }
 
-popupsAll?.addEventListener('click', switchPop)
+const closeClientPop = e => {
+	const elem = e.target
+	const check = elem.classList.contains('popups-create-client')
+	if (check) {
+		switchClientPop(false)
+	}
+}
+
+popupsCreateClient?.addEventListener('click', closeClientPop)
+buttonCloseAddClient?.addEventListener('click', () => switchClientPop(false))
+buttonOpenAddClient?.addEventListener('click',  () => switchClientPop(true))
