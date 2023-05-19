f887b0be553d53d1e5c28ae28f8e24b47a80de83
vasil <vasilbazhiv12@gmail.com>
2023-05-19 20:13:54 +0300

    new-order-win-pop

--- js/my-js.js
+++ js/my-js.js
@@ -3,10 +3,10 @@ const popupsAll = document.querySelector('.create-warehouse')
 const popupCreateBtn = document.querySelector('.create-tth')
 const popupCreateCloseBtn = document.querySelector('.create-warehouse__close ')
 
-function switchPopCreate (e){
-	if(e){
+function switchPopCreate(e) {
+	if (e) {
 		popupsAll.classList.add('popups-order-create')
-	}else{
+	} else {
 		popupsAll.classList.remove('popups-order-create')
 	}
 }
@@ -17,19 +17,21 @@ const switchPop = e => {
 		switchPopCreate(false)
 	}
 }
-popupCreateCloseBtn?.addEventListener('click', ()=> switchPopCreate(false) )
-popupCreateBtn?.addEventListener('click', ()=> switchPopCreate(true))
+popupCreateCloseBtn?.addEventListener('click', () => switchPopCreate(false))
+popupCreateBtn?.addEventListener('click', () => switchPopCreate(true))
 popupsAll?.addEventListener('click', switchPop)
 
 //client-pop
 const popupsCreateClient = document.querySelector('.popups-create-client')
 const buttonOpenAddClient = document.querySelector('.add-client-btn')
-const buttonCloseAddClient  = document.querySelector('.popups-create-client__close')
+const buttonCloseAddClient = document.querySelector(
+	'.popups-create-client__close'
+)
 
-function switchClientPop(a){
-	if(a){
+function switchClientPop(a) {
+	if (a) {
 		popupsCreateClient.classList.add('popups-create-client-active')
-	}else{
+	} else {
 		popupsCreateClient.classList.remove('popups-create-client-active')
 	}
 }
@@ -44,92 +46,156 @@ const closeClientPop = e => {
 
 popupsCreateClient?.addEventListener('click', closeClientPop)
 buttonCloseAddClient?.addEventListener('click', () => switchClientPop(false))
-buttonOpenAddClient?.addEventListener('click',  () => switchClientPop(true))
+buttonOpenAddClient?.addEventListener('click', () => switchClientPop(true))
 
 //plus-minus
 
 const ordersItemsAll = document.querySelector('.order-wind-items')
-const inputsResultAll = document.querySelectorAll('.input-result-calk') 
+const inputsResultAll = document.querySelectorAll('.input-result-calk')
 
-if(inputsResultAll){
+if (inputsResultAll) {
 	inputsResultAll.forEach(element => {
 		element.disabled = true
-	}); 
+	})
 }
 
-
-
-ordersItemsAll?.addEventListener('click', (e)=>{
+ordersItemsAll?.addEventListener('click', e => {
 	const elem = e.target
 	const plus = elem.classList.contains('btn-plus-calk')
 	const minus = elem.classList.contains('btn-minus-calk')
 	const input = elem.parentNode.querySelector('.order-item-input__input')
 	const num = Number(input.value)
-	if(plus){
-		input.value = num  + 1
+	if (plus) {
+		input.value = num + 1
 	}
-	if(minus && num){
-		input.value = num  - 1
+	if (minus && num) {
+		input.value = num - 1
 	}
 })
 //popup num-mail-address
 const popupMailTel = document.querySelector('.popup-mail-tel')
 const closeMailTel = document.querySelector('.popup-mail-tel__close')
-const popupTitle =	document.querySelector('.popup-mail-tel__title')
-//4 buttons
+const popupTitle = document.querySelector('.popup-mail-tel__title')
+const popupDesc = document.querySelector('.popup-mail-tel__sub-title')
+//buttons
 const openTellBtn = document.querySelector('.open-tell')
 const openAddressBtn = document.querySelector('.open-address')
 const openMailBtn = document.querySelector('.open-mail')
 const openTTHBtn = document.querySelector('.open-new-thh')
+const createSale = document.querySelector('.create-sale')
+const createCosts = document.querySelector('.create-costs')
+const openCreatePayment = document.querySelector('.open-create-payment')
+const renameClientAuto = document.querySelector('.rename-client-auto')
+const commentClientAuto = document.querySelector('.comment-client-auto')
+const allComment = document.querySelectorAll('.add-new-coment')
+const addBonusUah = document.querySelectorAll('.add-bonus-uah')
 
-
-
-
-
-function switchPopMailTel(title){
-	if(title){
+function switchPopMailTel(title, desc) {
+	if (title) {
 		popupMailTel.classList.add('popup-mail-tel-active')
 		popupTitle.textContent = title
-	}else{
+	} else {
 		popupMailTel.classList.remove('popup-mail-tel-active')
+	}
+	if(desc){
+		popupDesc.textContent = ''
+	}
+	if(title && !desc){
+		popupDesc.textContent = 'Витрати/додавання телефону і т.д'
 	}
 }
 
-function checkedInline (e){
+function checkedInline(e) {
 	const elem = e.target.classList.contains('popup-mail-tel')
-	if(elem){
+	if (elem) {
 		switchPopMailTel(false)
 	}
 }
 
 popupMailTel?.addEventListener('click', checkedInline)
-closeMailTel?.addEventListener('click', ()=> switchPopMailTel(false))
+closeMailTel?.addEventListener('click', () => switchPopMailTel(false))
 
+openTellBtn?.addEventListener('click', () =>
+	switchPopMailTel('Додати номер клієнта')
+)
+openAddressBtn?.addEventListener('click', () =>
+	switchPopMailTel('Додати адресу відділення')
+)
+openMailBtn?.addEventListener('click', () =>
+	switchPopMailTel('Додати пошту клієнта')
+)
+openTTHBtn?.addEventListener('click', () =>
+	switchPopMailTel('Додати існуючу ТТН')
+)
+createSale?.addEventListener('click', () =>
+	switchPopMailTel('Надати знижку', true)
+)
+createCosts?.addEventListener('click', () =>
+	switchPopMailTel('Створити витрату по замовленню', true)
+)
 
-openTellBtn?.addEventListener('click', ()=> switchPopMailTel('Додати номер клієнта'))
-openAddressBtn?.addEventListener('click', ()=> switchPopMailTel('Додати адресу відділення'))
-openMailBtn?.addEventListener('click', ()=> switchPopMailTel('Додати пошту клієнта'))
-openTTHBtn?.addEventListener('click', ()=> switchPopMailTel('Додати існуючу ТТН'))
+openCreatePayment?.addEventListener('click', () =>
+switchPopMailTel('Додати нову оплату', true)
+)
+renameClientAuto?.addEventListener('click', () =>
+switchPopMailTel('Назву авто клієнта', true)
+)
+commentClientAuto?.addEventListener('click', () =>
+switchPopMailTel('Комментар по авто клієнта', true)
+)
+
+allComment?.forEach(e =>{
+	e?.addEventListener('click', () =>
+	switchPopMailTel('Редагувати коментар', true)
+	)
+})
+
+addBonusUah?.forEach(e =>{
+	e?.addEventListener('click', () =>
+	switchPopMailTel('Додати бонус', true)
+	)
+})
+
 
 //communication with the client
+const allWindowClient = document.querySelectorAll('.communication-popup')
+const windowClientTell = document.querySelector('.communication-popup-tell')
+const windowClientMail = document.querySelector('.communication-popup-mail')
+const closeWindowClientTell = document.querySelector('.communication-btn-tel')
+const closeWindowClientMail = document.querySelector('.communication-btn-mail')
+const openWindowClientTell = document.querySelector('.btn-window-tell')
+const openWindowClientMail = document.querySelector('.btn-window-mail')
 
-const windowClient = document.querySelector('.communication-popup')
-const closeWindowClient = document.querySelector('.communication-popup__close')
 
-function switchClientWind (open){
-	if(open){
-		windowClient.classList.add('open-window-client')
-	}else{
-		windowClient.classList.remove('open-window-client')
+function switchClientWind(open, btn) {
+	console.log()
+	if (open) {
+		btn.classList.add('open-window-client')
+	} else {
+		btn.classList.remove('open-window-client')
 	}
 }
 
-function checkedWindClient(e){
-	const elem = e.target.classList.contains('communication-popup')
-	if(elem){
-		switchClientWind(false)
+function checkedWindClient(e) {
+	const elem = e.target
+	const checked = elem.classList.contains('communication-popup')
+	if (checked) {
+		switchClientWind(false, elem)
 	}
 }
+allWindowClient?.forEach(e => 
+	e.addEventListener('click', checkedWindClient)
+)
 
-windowClient.addEventListener('click', checkedWindClient)
-closeWindowClient.addEventListener('click', ()=> switchClientPop(false))
+closeWindowClientTell?.addEventListener('click', () =>
+	switchClientWind(false, windowClientTell)
+)
+closeWindowClientMail?.addEventListener('click', () =>
+	switchClientWind(false, windowClientMail)
+)
+openWindowClientTell?.addEventListener('click', () =>
+	switchClientWind(true, windowClientTell)
+)
+openWindowClientMail?.addEventListener('click', () =>
+	switchClientWind(true, windowClientMail)
+)
